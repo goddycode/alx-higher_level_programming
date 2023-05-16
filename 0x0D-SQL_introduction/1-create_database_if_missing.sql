133c0daca07a119d4f0594730f431f8093e91ab2
godycode <odhisgoddy24@gmail.com>
2023-05-16 09:52:52 +0000

    '-create_database_if_missing.sql' #3 commit

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- This script creates the database hbtn_0c_0 in my MySQL server and does not fail if it exist.
-CREATE DATABASE IF NOT EXIST `hbtn_0c_0`;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
