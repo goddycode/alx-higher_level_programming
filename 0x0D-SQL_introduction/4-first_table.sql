3914c11193e6d2ab413aaaf3390a1ffec31c62c9
godycode <odhisgoddy24@gmail.com>
2023-05-16 12:46:22 +0000

    '4-first_table.sql' commit#2

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
 -- This script creates a table called 'first_table' in current MYSQL server
 
-CREATE TABLE IF NOT EXISTS first_table(it INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
