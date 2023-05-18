020b811eb0ee1727a1a407d5ea06186b60cd9fe9
godycode <odhisgoddy24@gmail.com>
2023-05-18 10:45:17 +0000

    '12-no_genre.sql' file commit #2

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,3 +1,3 @@
 -- script that lists all shows contained in hbtn_0d_tvshows without a genre linked.
 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows OUTER JOIN tv_show_genres ON tv_show_genres.genre_id IS NULL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_show_genres.genre_id IS NULL
