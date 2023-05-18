7e9e46680fd28866f43f6caf5e2d4d87af30b835
godycode <odhisgoddy24@gmail.com>
2023-05-18 04:54:17 +0000

    '6-states.sql' file commit #5

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,10 +1,7 @@
--- Script which creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on my MySQL server
-
--- Creates database hbtn_0d_usa
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- Use hbtn_0d_usa
+-- use a database
 USE hbtn_0d_usa;
-
--- Create table states
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL), PRIMARY KEY(id));
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
