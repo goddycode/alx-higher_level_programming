17b7dab5100ecf68c43e2bc9fe4025d50e36ce0c
godycode <odhisgoddy24@gmail.com>
2023-05-18 04:30:53 +0000

    '5-unique_id.sql' file commit #1

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,3 @@
 -- Script that creates the table unique_id on your MySQL server.
 
-CREATE TABLE IF NOT EXISTS (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
