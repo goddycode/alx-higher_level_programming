0356e626bd618ce3c5be5ef9833af2c7177c233f
godycode <odhisgoddy24@gmail.com>
2023-05-18 07:28:09 +0000

    '9-cities_by_state_join.sql' file commit #4

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Script which list all cities contained in the database hbtn_0d_usa
 -- List all rows of some columns from database
 
-SELECT cities.id, cities.name, states.id FROM cities JOIN states ON cities.states_id = states.id ORDER BY cities.id ASC;
+SELECT cities.id, cities.name, states.name FROM cities LEFT JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC;
