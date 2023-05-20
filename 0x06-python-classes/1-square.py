95409cf7875fa2b90182bb05c36ffc76d23b3ba4
godycode <odhisgoddy24@gmail.com>
2023-05-20 09:00:47 +0000

    0x06-python-classes'1-square.py'

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""defining a square by private instance attribute: size
 
 
 class Square:
-    """Represent a square."""
-
-    def __init__(self, size):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.__size = size
+    """REpresenting square class"""
+    __size ='None;
