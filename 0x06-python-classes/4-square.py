e933d335cae0a523a1579a151b57c91efa566887
godycode <odhisgoddy24@gmail.com>
2023-05-20 12:12:22 +0000

    0x06-python-classes'4-square.py' explore commit #7

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -14,10 +14,11 @@ class Square:
 
     @property
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
-    def (size, value):
+    def size(size, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif value < 0:
