cdac3bb82db5f76bf20ab1935ff0611d32a639a8
godycode <odhisgoddy24@gmail.com>
2023-05-20 11:12:38 +0000

    0x06-python-classes'4-square.py' explore commit #1

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,31 +1,27 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
+"""Defining a square by: (based on 3-square.py"""
 
 
 class Square:
-    """Represent a square."""
+"""Represent square class"""
 
-    def __init__(self, size=0):
-        """Initialize a new square.
-        Args:
-            size (int): The size of the new square.
-        """
-        self.size = size
+    def __init__(self, size):
+        """Initiating class"""
+        
+        @property
+        def size(self):
+            rrturn self.__size
 
-    @property
-    def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
-
-    @size.setter
-    def size(self, value):
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = value
+        @size.setter
+        def size, value):
+            if not isinstance(value, int):
+                raise TypeError("size must be an integer")
+            elif value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
 
     def area(self):
-        """Return the current area of the square."""
-        return (self.__size * self.__size)
+    """working on area of a square"""
+
+        return self.__size ** 2
