e68e4372224798953f164a718a53378a836785a7
godycode <odhisgoddy24@gmail.com>
2023-05-20 10:14:15 +0000

    0x06-python-classes'2-square.py' explore commit #10

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,7 +9,7 @@ class Square:
     def __init__(self, size=0):
         """Initialize a new square.
         Args:
-        size (int): The size of the new square
+        size (int): The size of the new square"""
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
