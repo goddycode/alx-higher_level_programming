03f1c4502a57b336540696b50d049b75434c0b45
godycode <odhisgoddy24@gmail.com>
2023-05-20 09:26:51 +0000

    0x06-python-classes'2-square.py' explore commit #4

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 
-"""Define a class Square."""
-
+""" Defining a square class  by: (based on 1-square.py)"""
 
 class Square:
-    """Represent a square."""
-
+    """Class initiation"""
+    
     def __init__(self, size=0):
-        """Initialize a new Square.
-        Args:
-            size (int): The size of the new square.
-        """
-        if not isinstance(size, int):
+        self.size = size
+
+    """Getter"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, val):
+        if val.isdigit():
+            if val < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                return self.__size
+        else:
             raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+
