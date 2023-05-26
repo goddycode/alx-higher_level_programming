1b4aba1d2fe6329beaac619745aedf4953f48fad
godycode <odhisgoddy24@gmail.com>
2023-05-26 08:04:36 +0000

    '3-rectangle.py ' explore commit#2

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -47,7 +47,7 @@ class Rectangle:
         if (self.__width == 0 or self.__height == 0):
             return (0)
         return ((2 * self.__width) + (self.__height * 2))
-    if (self.width == 0 or self.height == 0):
+    if (self.__width == 0 or self.__height == 0):
         print("")
     for i in range(self.__width):
         for j in range(self.__height):
