56e1c3e09b48fba8c817079fa52d3ed1cffd4e33
godycode <odhisgoddy24@gmail.com>
2023-05-26 07:04:57 +0000

    '2-rectangle.pywq' explore commit#1

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+
+"""
+class Rectangle that defines a rectangle
+by: (based on 1-rectangle.py)
+"""
+
+
+class Rectangle:
+    """Defining Rectange class"""
+
+    def __init__(self, width=0, height=0):
+        """initiation method"""
+        self.width = width
+        self.height = height
+
+    @property width
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property height
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """public instance method that returns the rectangle area"""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """public instance method that returns the rectangle perimeter"""
+        return (self.__width + self.__height)
+        if (self.__width == 0 or self.__height == 0):
+            print("")
