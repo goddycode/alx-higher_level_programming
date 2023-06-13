ba0baf18b2c11e8050aeb771ee0c66aad914f2b8
goddycode <odhisgoddy24@gmai.com>
2023-06-13 13:59:21 +0000

    Adding all other files to '0x12-javascript-warm_up' commit 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,11 +5,4 @@ if (process.argv.length === 2) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
-}#!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
 }
