9faaf27bab2c649017f64281da6bc6cbd544e344
goddycode <odhisgoddy24@gmail.com>
2023-06-19 02:49:31 +0000

    Debuggig task 11 commit #2

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
-'''Scripts which add State Object and print the new states.id after creation
-'''
-import sys
+"""
+a script that adds the State object “Louisiana” to the
+database hbtn_0e_6_usa
+"""
+
+from sys import argv
 from model_state import Base, State
-from sqlachemy import (create_engine)
-from sqlachemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    new_added_state = State(name='Louisiana')
-    session.add(new_added_state)
-    new_instance = session.query(State).filter_by(name='Louisiana'.first()
-    print(new_instance.id)
+
+    # add new state and commit to table
+    new = State(name="Louisiana")
+    session.add(new)
     session.commit()
+
+    print("{:d}".format(new.id))
+
+    session.close()
