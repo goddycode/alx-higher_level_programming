c964a2de91ce4799301171f6d6db436ec93e00a8
goddycode <odhisgoddy24@gmail.com>
2023-06-19 02:42:53 +0000

    Debuggig task 10 commit #3

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,28 +1,25 @@
-#!/usr/bin/python3
-"""Prints the first State object from the database hbtn_0e_6_usa"""
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python3 script_name.py db_username db_password db_name state_name")
-        sys.exit(1)
 
-    db_username = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-
-    engine = create_engine(f'mysql+mysqldb://{db_username}:{db_password}@localhost:3306/{db_name}')
+    # make engine for database
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(user, passwd, db), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    instance = session.query(State).filter_by(name=state_name).first()
-    if instance:
-        print(instance.id)
+    # query python instance in database state id given state name
+    state = session.query(State).filter_by(name=argv[4]).first()
+    if state:
+        print("{:d}".format(state.id))
     else:
-        print("State not found")
+        print("Not found")
+    session.close()
