1f057eef76c057affdff5e316b0bad853d5177f6
vagrant <vagrant@ubuntu2004.localdomain>
2023-06-30 11:14:46 +0000

    Debugging '102-catch_me.sh'wq file commit #5

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script which makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with corresponded message
-curl -s -o response.txt 0.0.0.0:5000/catch_me
+curl -s -X PUT -L -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
