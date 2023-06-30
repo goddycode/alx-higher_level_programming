5ff87b887b4644ac1b16e35da30046e0929b5bac
vagrant <vagrant@ubuntu2004.localdomain>
2023-06-30 10:22:32 +0000

    Debugging 6-peak.py file commit #2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-""" The script in this module returns the peak of the list
+""" The script will return from list of int passed
 """
 
 
-def find_peak(tobe_testedl_list_of_integers):
-    """ This function returns the peak of the list
+def find_peak(list_of_integers):
+    """ find_peak function return peak
     """
-    if (len(tobe_testedl_list_of_integers) == 0):
+    if (len(list_of_integers) == 0):
         return None
 
     else:
-        baseline = tobe_testedl_list_of_integers[0]
-        for i in range(len(tobe_testedl_list_of_integers)):
-            if tobe_testedl_list_of_integers[i] > baseline:
-                baseline = tobe_testedl_list_of_integers[i]
-        return baseline
+        mypeak = list_of_integers[0]
+        for i in range(len(list_of_integers)):
+            if list_of_integers[i] > list_of_integers:
+                 mypeak = list_of_integers[i]
+        return  mypeak
