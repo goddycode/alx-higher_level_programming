d018a804c460c4dd97c4717671b874da1d5a1c57
vagrant <vagrant@ubuntu2004.localdomain>
2023-06-30 08:53:55 +0000

    Adding '3-methods.sh' file to '0x10-python-network_0' repo'

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Script which takes in a URL, sends a GET request to the URL, and 
 #displays the body of the response
 curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,4 @@
-
-#!/usr/bin/env bash
+#!/bin/bash
 # Scripts which sends a DELETE request to the URL passed as the first
 # argument and displays the body of the response
 curl -sX "DELETE" "$1"
