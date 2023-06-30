611a60c412f1a361ba77f1fdc2a30e6dafb1b069
vagrant <vagrant@ubuntu2004.localdomain>
2023-06-30 09:58:20 +0000

    Debugging task number 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script which makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
-curl -s 0.0.0.0:5000/catch_me -X PUT -L -d "user_id=98" -H "Origin: HolbertonSchool"1~
+curl -s -o response.txt 0.0.0.0:5000/catch_me
