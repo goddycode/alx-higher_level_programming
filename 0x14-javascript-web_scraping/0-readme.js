2dfd5adf44c340ac412620bc5bb7483bea78cd5c
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-25 09:28:55 +0000

    Adding '0-readme.js' file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-
-const fs = require('fs').promises;
-
-async function readAndPrintFile(filePath) {
-  try {
-    const data = await fs.readFile(filePath, 'utf8');
-    process.stdout.write(data);
-  } catch (err) {
-    console.error('Error:', err.message);
-    process.exit(1);
-  }
-}
-
-if (process.argv.length !== 3) {
-  console.log('Usage: node read_file.js <file_path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-readAndPrintFile(filePath);
-
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
