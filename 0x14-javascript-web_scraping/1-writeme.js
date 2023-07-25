a66b1138334feb0e04a4f4006edad7bdec8c68b2
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-25 11:58:20 +0000

    zdebugging '1-writeme.js' commit #3

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,19 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.log('Usage: node write_file.js <file_path> <string_to_write>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
-
-fs.writeFile(filePath, contentToWrite, 'utf8', function (error) {
-  if (error) {
-    console.error('Error:', error);
-  } else {
-    console.log('Content written to the file successfully.');
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
