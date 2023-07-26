2604522e6a696de83f6b2e2a684a89fd583b1882
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-26 06:15:25 +0000

    Debugging '3-starwars_title.js' commit #2

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 
-const re = require('re');
+const request = require('request');
 const episodeNum = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-request(API_URL + episodeNum, function (error, response, body) {
-  if (error) {
-    console.log(error);
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
   } else if (response.statusCode === 200) {
     const responseJSON = JSON.parse(body);
     console.log(responseJSON.title);
