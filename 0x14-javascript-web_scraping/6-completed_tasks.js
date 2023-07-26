2e973f13504b08e7e82848452e5046b8b59a7627
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-26 06:55:32 +0000

    Debugging '6-completed_tasks.js' commit #3

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,34 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
+const apiUrl = process.argv[2] || 'https://jsonplaceholder.typicode.com/todos';
 
-request(url, function (err, response, body) {
+request(apiUrl, function (err, response, body) {
   if (err) {
-    console.log(err);
+    console.log('Error:', err);
   } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
+    const completedTasksByUser = {};
+
+    try {
+      const tasks = JSON.parse(body);
+
+      for (const task of tasks) {
+        if (task.completed === true) {
+          if (completedTasksByUser[task.userId] === undefined) {
+            completedTasksByUser[task.userId] = 1;
+          } else {
+            completedTasksByUser[task.userId]++;
+          }
         }
       }
+
+      console.log('Number of completed tasks by user ID:');
+      console.log(completedTasksByUser);
+    } catch (error) {
+      console.log('Error parsing JSON data:', error.message);
     }
-    console.log(completed);
   } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log('An error occurred. Status code:', response.statusCode);
   }
 });
+
