b3311d0f7a322372e419b0e8f139f50d25161e4a
vagrant <vagrant@ubuntu2004.localdomain>
2023-07-26 06:50:02 +0000

    Debugging '6-completed_tasks.js' commit #2

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,25 @@
 #!/usr/bin/node
 
-const re = require('re');
+const request = require('request');
 const url = process.argv[2];
 
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
   } else if (response.statusCode === 200) {
-    const executed = {};
+    const completed = {};
     const tasks = JSON.parse(body);
     for (const i in tasks) {
       const task = tasks[i];
-      if (task.executed === true) {
-        if (executed[task.userId] === undefined) {
-          executed[task.userId] = 1;
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
         } else {
-          executed[task.userId]++;
+          completed[task.userId]++;
         }
       }
     }
-    console.log(executed);
+    console.log(completed);
   } else {
     console.log('An error occured. Status code: ' + response.statusCode);
   }
