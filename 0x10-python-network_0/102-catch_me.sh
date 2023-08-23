a9086e76f68c71298b38547f94ac4b48defec5be
vagrant <vagrant@ubuntu2004.localdomain>
2023-08-23 10:48:00 +0000

    month 6-8 tasks for alx course defrement commit #2

--- 0x10-python-network_0/102-catch_me.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Script which makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with corresponded message
-curl -s -X PUT -L -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.printNumber = function(arg) {
-  if (!isNaN(parseInt(arg))) {
-    console.log("My number:", parseInt(arg));
-  } else {
-    console.log("Not a number");
-  }
-};
