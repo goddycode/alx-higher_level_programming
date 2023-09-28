a33d70c2e107ee5a5e24768af08c79e9f86dbda7
goddycode <odhisgoddy24@gmail.com>
2023-09-28 13:35:27 +0000

    Updating '6-peak.py  ' commit 2

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
-"""
-Function that finds a peak in a list of unsorted integers
-"""
+"""This module contains a script that finds a peak in a list of unsorted
+integers"""
 
 
 def find_peak(list_of_integers):
-    """
-    finds num that's greater than both left and right
-    """
-    if len(list_of_integers) == 0:
+    """Finds a peak in a list of unsorted integers"""
+    length = len(list_of_integers)
+    if length == 0:
         return None
+    peak = re_peak(list_of_integers, 0, length - 1)
+    return peak
 
-    lists = list_of_integers
-    beg = 0
-    end = len(lists)-1
 
+def re_peak(int_list, start, end):
+    if start == end:
+        return int_list[start]
+    mid = int((start + end) // 2)
+    if int_list[mid] > int_list[mid + 1] and int_list[mid] > int_list[mid - 1]:
+        return int_list[mid]
+    if int_list[mid + 1] > int_list[mid]:
+        return re_peak(int_list, mid + 1, end)
+    else:
+        return re_peak(int_list, 0, mid - 1)
