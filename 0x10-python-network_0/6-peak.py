e0e936e5e8b14a950113eb38f72ec8ed6c14e086
goddycode <odhisgoddy24@gmail.com>
2023-09-28 13:10:43 +0000

    Updating '6-peak.py  '

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Function that finds a peak in a list of unsorted integers
+"""
+
+
+def find_peak(list_of_integers):
+    """
+    finds num that's greater than both left and right
+    """
+    if len(list_of_integers) == 0:
+        return None
+
+    lists = list_of_integers
+    beg = 0
+    end = len(lists)-1
+
