f7cc10e7c518c1f0f341af44534cd2dbad99e172
goddycode <odhisgoddy24@gmail.com>
2023-09-28 13:33:14 +0000

    Updating '102-catch_me.sh  ' commit 3

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-
-# Make a request to the specified URL
-response=$(curl -s -X PUT -d "user_id=98" "http://0.0.0.0:5000/catch_me")
-
-# Since you are not allowed to use echo or cat, we'll simply exit with the response
-# The response will be displayed as the script's exit code, which can be checked
-
-# Check if the request was successful
-if [ $? -eq 0 ]; then
-    exit "$response"
-else
-    exit 1  # Error occurred
-fi
-
+# curl url till we get "You got me!"
+curl -sL http://0.0.0.0:5000/catch_me -X PUT -d "user_id=98" -H "Origin: HolbertonSchool"
