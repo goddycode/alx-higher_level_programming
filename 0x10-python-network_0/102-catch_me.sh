8d8161c63235f75ce20b4b22bc2064d463da7f45
goddycode <odhisgoddy24@gmail.com>
2023-09-28 13:29:25 +0000

    Adding '102-catch_me.sh  '

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# Script which makes a request to 0.0.0.0:5000/catch_me that gets the message "You got me!".
-curl -s -X PUT -L -d "user_id=98" -H "You got me!" 0.0.0.0:5000/catch_me
+
+# Make a request to the specified URL
+response=$(curl -s -X PUT -d "user_id=98" "http://0.0.0.0:5000/catch_me")
+
+# Since you are not allowed to use echo or cat, we'll simply exit with the response
+# The response will be displayed as the script's exit code, which can be checked
+
+# Check if the request was successful
+if [ $? -eq 0 ]; then
+    exit "$response"
+else
+    exit 1  # Error occurred
+fi
+
