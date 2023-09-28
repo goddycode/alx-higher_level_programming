f61c75b1dfe1b906c4b7d1895315caa9a06ac302
goddycode <odhisgoddy24@gmail.com>
2023-09-28 13:38:03 +0000

    Updating '0-body_size.sh  ' commit 2

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Script which takes in a URL, sends a request to
-# that URL, and displays the size of the body of the response size of body
-# of response; Usage: ./0-body_size.sh 0.0.0.0:5000
-curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
+#takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI "$1" | grep 'Content-Length:' | cut -d' ' -f2
