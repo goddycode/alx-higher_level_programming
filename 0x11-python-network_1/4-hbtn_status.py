8042c6f02e992c1d132b3b315cbb816e3bf57e06
goddycode <odhisgoddy24@gmail.com>
2023-09-30 01:52:56 +0000

    Adding '4-hbtn_status.py  '

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import requests
+
+"""fetches https://alx-intranet.hbtn.io/status."""
+
+
+if __name__ == "__main__":
+    r = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
