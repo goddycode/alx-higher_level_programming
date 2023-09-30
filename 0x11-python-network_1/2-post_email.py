d4814d6496d1148d506603e93f77fb65c7651af1
goddycode <odhisgoddy24@gmail.com>
2023-09-30 02:29:18 +0000

    Updating ' 2-post_email.py   '

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-import sys
+"""
+Takes URL & email as params, send POST req to URL, display response body utf-8
+usage: ./2-post_email.py http://0.0.0.0:5000/post_email hr@holbertonschool.com
+"""
+from sys import argv
 import urllib.parse
 import urllib.request
 
-"""Python script that takes in a URL and an email,"""
-"""sends a POST request to the passed"""
-"""URL with the email as a parameter, and"""
-"""displays the body of the response (decoded in utf-8)
-"""
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    value = {"email": sys.argv[2]}
-    data = urllib.parse.urlencode(value).encode("ascii")
+    url = argv[1]
+    values = {'email': argv[2]}
 
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))
