c08cd22b60276043d2c5beb37ba3e5ad78731e71
goddycode <odhisgoddy24@gmail.com>
2023-09-30 02:27:44 +0000

    Updating ' 1-hbtn_header.py   '

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-import sys
-from urllib import request
-
-"""Script which takes in a URL, send request to URL"""
-""" and display value of `X-Request-Id`"""
+"""
+Taskes URL as parameter, fetch URL and display value from reponse header
+usage: ./1-hbtn_header https://intranet.hbtn.io
+"""
+from sys import argv
+import urllib.request
 
 
 if __name__ == "__main__":
-    with request.urlopen(sys.argv[1]) as res:
-        print(res.info()['X-Request-Id'])
+    req = urllib.request.Request(argv[1])
+    with urllib.request.urlopen(req) as response:
+        print(response.getheader('X-Request-Id'))
