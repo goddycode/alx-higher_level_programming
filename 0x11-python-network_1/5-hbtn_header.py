d889d1f11133937db2685257f452e4d64e851ee5
goddycode <odhisgoddy24@gmail.com>
2023-09-30 02:34:24 +0000

    Updating ' 5-hbtn_header.py   '

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""
+Takes URL as parameter, fetch URL and display value from reponse header
+usage: ./5-hbtn_header https://intranet.hbtn.io
+"""
+from sys import argv
 import requests
-import sys
 
-'''task 5 script'''
 
-if __name__ == '__main__':
-
-    res = requests.get(sys.argv[1])
-    print(res.headers.get('X-Request-Id'))
+if __name__ == "__main__":
+    r = requests.get(argv[1])
+    print(r.headers.get('X-Request-Id'))
