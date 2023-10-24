8251ad38255e75db8945051f9bfa1d27c077cb02
goddycode <odhisgoddy24@gmail.com>
2023-10-24 23:05:52 +0000

    Updating '1-writeme.js' commit #3

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const myfile = process.argv[2];
 const mytext = process.argv[3];
 const fs = require('fs');
 fs.writeFile(myfile, mytext, 'utf8', function (myerror) {
-if (myerror) {
+  if (myerror) {
     console.log(myerror);
   }
 });
