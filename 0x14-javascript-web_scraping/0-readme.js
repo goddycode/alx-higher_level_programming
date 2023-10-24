b8fe06da3b2ec5130794cff8d4cf220390d5f8e9
goddycode <odhisgoddy24@gmail.com>
2023-10-24 22:53:33 +0000

    Updating '0-readme.js' commit #2

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,10 +3,9 @@
 This scripts reads and prints the content of a file
 */
 
+
 const myFile = process.argv[2];
-
 const fs = require('fs');
-
 fs.readFile(myFile, 'utf8', function (myError, myData) {
   if (myError) {
     console.log(myError);
