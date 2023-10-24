b24cfebe8d730f04f1b00f6c6ef3d8384a639cf4
goddycode <odhisgoddy24@gmail.com>
2023-10-24 23:24:55 +0000

    Adding '2-statuscode.js' commit #1

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const myUrl = process.argv[2];
+const request = require('request');
+
+request(myUrl, function (myError, response) {
+  if (myError){
+    console.log(myError);
+  } else {
+    console.log('code: ' +response.statusCode);
+  }
+}); 
