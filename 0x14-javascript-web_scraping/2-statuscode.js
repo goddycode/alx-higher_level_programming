47aa8b247c438344a72ee4c6617e6ff18e019fbd
goddycode <odhisgoddy24@gmail.com>
2023-10-24 23:32:55 +0000

    Updating '2-statuscode.js' commit #3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const myUrl = process.argv[2];
 const request = require('request');
 request(myUrl, function (myError, response) {
-  if (myError){
+  if (myError) {
     console.log(myError);
   } else {
     console.log('code: ' + response.statusCode);
