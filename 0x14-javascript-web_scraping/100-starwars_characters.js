6923cc8604942cdf6e66f583378d721376f5c2d1
goddycode <odhisgoddy24@gmail.com>
2023-10-25 00:39:47 +0000

    Adding '100-starwars_characters.js ' commit #1

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+const myUrl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(myUrl, function (myError, response, body) {
+  if (!myError) {
+    const characters = JSON.parse(body).characters;
+    characters.forEach((character) => {
+      request(character, function (myError, response, body) {
+        if (!myError) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    });
+  }
+});01~
